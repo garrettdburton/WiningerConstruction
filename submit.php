a3e4cc12f18ad48a2236dc304a072aa45807a040
Garrett Burton <garrettdburton@gmail.com>
2024-12-16 15:42:22 -0500

    updates

--- submit.php
+++ submit.php
@@ -9,10 +9,30 @@ $interior = $_POST['interior'];
 $upgrade_bath_accessories = $_POST['upgrade_bath_accessories'];
 $upgrade_light_fixtures = $_POST['upgrade_light_fixtures'];
 $upgrade_kitchen_backsplash = $_POST['upgrade_kitchen_backsplash'];
-/*Insert Test*/
+$emailMessage =
+	'Customer Name: ' . $name . PHP_EOL 
+. 'Customer Email: ' . $email . PHP_EOL 
+. 'Customer Phone: ' . $phone . PHP_EOL 
+. 'Floorplan: ' . $floorplan . PHP_EOL 
+. 'Exterior: ' . $exterior . PHP_EOL
+. 'Interior: ' . $interior . PHP_EOL
+. 'Upgrade Bath: ' . $upgrade_bath_accessories . PHP_EOL
+. 'Upgrade Light: ' . $upgrade_light_fixtures . PHP_EOL
+. 'Upgrade Backsplash: ' . $upgrade_bath_accessories . PHP_EOL;
+
+
+
+$emailMessage = wordwrap($emailMessage, 70);
+
+/* Insert Test */
 $sql = "INSERT INTO customer (id, date, name, email, phone, floorplan, exterior, interior, upgrade_bath_accessories, upgrade_light_fixtures, upgrade_kitchen_backsplash)
 VALUES (DEFAULT, Now(),'$name', '$email', '$phone', '$floorplan', '$exterior', '$interior', '$upgrade_bath_accessories', '$upgrade_light_fixtures', '$upgrade_kitchen_backsplash')";
 
+
+/* Send email */
+mail('thenorthsider35@gmail.com', 'New Customer Request', $emailMessage);
+
+/* Confirmation of completed actions */
 if ($conn->query($sql) === TRUE) {
 	echo "New record created successfully <br>";
 } else {
