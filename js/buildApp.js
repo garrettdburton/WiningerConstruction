3ab3acbd8f57a829702586f985dc71061c06fef7
Garrett Burton <garrettdburton@gmail.com>
2025-01-09 10:52:00 -0500

    Updates

--- js/buildApp.js
+++ js/buildApp.js
@@ -77,7 +77,7 @@ function fixStepIndicator(n) {
 let floorplanName, exteriorName, interiorName;
 
 // This function sets form variables (name, bed, bath, floors, garage, features)
-function setFloorplan(image, name, bed, bath, floor, garage, feature, space, price) {
+function setFloorplan(image, name, bed, bath, floor, garage, feature, space, video) {
   floorplanName = name;
   let floorplanImage = image;
   let floorplanBed = bed;
@@ -86,7 +86,7 @@ function setFloorplan(image, name, bed, bath, floor, garage, feature, space, pri
   let floorplanGarage = garage;
   let floorplanFeat = feature;
   let floorplanSpace = space;
-  let floorplanPrice = price;
+  let floorplanVideo = video;
   $("#floorplanImageSelected").attr('src', floorplanImage);
   $("#floorplanNameSelected").text(floorplanName);
   $("#floorplanBedSelected").text(floorplanBed);
@@ -95,8 +95,47 @@ function setFloorplan(image, name, bed, bath, floor, garage, feature, space, pri
   $("#floorplanGarageSelected").text(floorplanGarage);
   $("#floorplanFeatSelected").text(floorplanFeat);
   $("#floorplanSpaceSelected").text(floorplanSpace);
-  $("#floorplanPriceSelected").text(floorplanPrice);
-  console.log(floorplanImage, floorplanName, floorplanBed, floorplanBath, floorplanFloor, floorplanGarage, floorplanFeat, floorplanSpace, floorplanPrice);
+  console.log(floorplanImage, floorplanName, floorplanBed, floorplanBath, floorplanFloor, floorplanGarage, floorplanFeat, floorplanSpace, floorplanVideo);
+
+  if (floorplanVideo == "town_single") {
+    $("#floorplanVideoSelected").text("See Inside")
+    $("#floorplanVideoSelected").attr("class", "visible");
+    $("#floorplanVideoSelected").attr("target", "_blank");
+    $("#floorplanVideoSelected").attr("href", "https://youtu.be/ck0f05qOZB0");
+  } 
+
+  if (floorplanVideo == "town_pair") {
+    $("#floorplanVideoSelected").text("See Inside")
+    $("#floorplanVideoSelected").attr("class", "visible");
+    $("#floorplanVideoSelected").attr("target", "_blank");
+    $("#floorplanVideoSelected").attr("href", "https://youtu.be/6x9TBKvdGdg");
+  } 
+
+  if (floorplanVideo == "lg_ranch_single") {
+    $("#floorplanVideoSelected").text("See Inside")
+    $("#floorplanVideoSelected").attr("class", "visible");
+    $("#floorplanVideoSelected").attr("target", "_blank");
+    $("#floorplanVideoSelected").attr("href", "https://youtu.be/Xw_MihnIuKs");
+  } 
+
+  if (floorplanVideo == "lg_ranch_paired") {
+    $("#floorplanVideoSelected").text("See Inside")
+    $("#floorplanVideoSelected").attr("class", "visible");
+    $("#floorplanVideoSelected").attr("target", "_blank");
+    $("#floorplanVideoSelected").attr("href", "https://youtu.be/veLy4EcGjoc");
+  } 
+
+  if (floorplanVideo == "sm_ranch_single") {
+    $("#floorplanVideoSelected").attr("class", "invisible");
+  } 
+
+  if (floorplanVideo == "sm_ranch_paired") {
+    $("#floorplanVideoSelected").attr("class", "invisible");
+  } 
+  
+  
+
+  console.log(floorplanImage, floorplanName, floorplanBed, floorplanBath, floorplanFloor, floorplanGarage, floorplanFeat, floorplanSpace, floorplanVideo);
 }
 
 // This function sets form variables
@@ -190,64 +229,64 @@ function setInterior(image, name, cabinetTop, cabinetTopImage, cabinetBottom, ca
 }
 
 // accessory function
-    // Function to clear check boxes after none is selected
-    function clearChecks() {
-      var noAcc = document.getElementById("noAcc").checked;
+// Function to clear check boxes after none is selected
+function clearChecks() {
+  var noAcc = document.getElementById("noAcc").checked;
 
-      // disable check boxes
-      if (noAcc) { 
-        console.log("true"); 
-        $("#accpackage1").prop( "checked", false);
-        $("#accpackage2").prop( "checked", false);
-        $("#accpackage3").prop( "checked", false);
+  // disable check boxes
+  if (noAcc) {
+    console.log("true");
+    $("#accpackage1").prop("checked", false);
+    $("#accpackage2").prop("checked", false);
+    $("#accpackage3").prop("checked", false);
 
-        $("#accpackage1").attr("disabled", true);
-        $("#accpackage2").attr("disabled", true);
-        $("#accpackage3").attr("disabled", true);
-        console.log("Done");
-      }
-      // enable check boxes
-      if (!noAcc) { 
-        $("#accpackage1").attr("disabled", false);
-        $("#accpackage2").attr("disabled", false);
-        $("#accpackage3").attr("disabled", false);
-        console.log("Done");
-      }
-    }
+    $("#accpackage1").attr("disabled", true);
+    $("#accpackage2").attr("disabled", true);
+    $("#accpackage3").attr("disabled", true);
+    console.log("Done");
+  }
+  // enable check boxes
+  if (!noAcc) {
+    $("#accpackage1").attr("disabled", false);
+    $("#accpackage2").attr("disabled", false);
+    $("#accpackage3").attr("disabled", false);
+    console.log("Done");
+  }
+}
 
-    //function to control the exterior package page
-    var enteriorCarouselIndicator = 0;
+//function to control the exterior package page
+var enteriorCarouselIndicator = 0;
 
-    $(".carousel-link").click(function () {
-      $(".carousel-link").attr('style', 'color: white !important; text-decoration: none !important');
-      $(this).attr('style', 'color: #f9bf29 !important');
-      enteriorCarouselIndicator = this.id.slice(-1);
-      enteriorCarouselIndicator = parseInt(enteriorCarouselIndicator, 10);
-    });
+$(".carousel-link").click(function () {
+  $(".carousel-link").attr('style', 'color: white !important; text-decoration: none !important');
+  $(this).attr('style', 'color: #f9bf29 !important');
+  enteriorCarouselIndicator = this.id.slice(-1);
+  enteriorCarouselIndicator = parseInt(enteriorCarouselIndicator, 10);
+});
 
-    function incrementEntWithArrow() {
-      enteriorCarouselIndicator = enteriorCarouselIndicator + 1;
-      if (enteriorCarouselIndicator > 4) {
-        enteriorCarouselIndicator = 0;
-        $(".carousel-link").attr('style', 'color: white !important; text-decoration: none !important');
-        $('#ent' + enteriorCarouselIndicator).attr('style', 'color: #f9bf29 !important');
-      } else {
-        $(".carousel-link").attr('style', 'color: white !important; text-decoration: none !important');
-        $('#ent' + enteriorCarouselIndicator).attr('style', 'color: #f9bf29 !important');
-      }
-    };
+function incrementEntWithArrow() {
+  enteriorCarouselIndicator = enteriorCarouselIndicator + 1;
+  if (enteriorCarouselIndicator > 4) {
+    enteriorCarouselIndicator = 0;
+    $(".carousel-link").attr('style', 'color: white !important; text-decoration: none !important');
+    $('#ent' + enteriorCarouselIndicator).attr('style', 'color: #f9bf29 !important');
+  } else {
+    $(".carousel-link").attr('style', 'color: white !important; text-decoration: none !important');
+    $('#ent' + enteriorCarouselIndicator).attr('style', 'color: #f9bf29 !important');
+  }
+};
 
-    function decrementEntWithArrow() {
-      if (enteriorCarouselIndicator == 0) {
-        enteriorCarouselIndicator = 4;
-        $(".carousel-link").attr('style', 'color: white !important; text-decoration: none !important');
-        $('#ent' + enteriorCarouselIndicator).attr('style', 'color: #f9bf29 !important');
-      } else {
-        enteriorCarouselIndicator = enteriorCarouselIndicator - 1;
-        $(".carousel-link").attr('style', 'color: white !important; text-decoration: none !important');
-        $('#ent' + enteriorCarouselIndicator).attr('style', 'color: #f9bf29 !important');
-      }
-      console.log(enteriorCarouselIndicator);
-    };
+function decrementEntWithArrow() {
+  if (enteriorCarouselIndicator == 0) {
+    enteriorCarouselIndicator = 4;
+    $(".carousel-link").attr('style', 'color: white !important; text-decoration: none !important');
+    $('#ent' + enteriorCarouselIndicator).attr('style', 'color: #f9bf29 !important');
+  } else {
+    enteriorCarouselIndicator = enteriorCarouselIndicator - 1;
+    $(".carousel-link").attr('style', 'color: white !important; text-decoration: none !important');
+    $('#ent' + enteriorCarouselIndicator).attr('style', 'color: #f9bf29 !important');
+  }
+  console.log(enteriorCarouselIndicator);
+};
 
 
